88b5a82362de591fced50e84affc9e28b48317c6
Boris Mocialov <iphoneetc@gmail.com>
2021-03-01 13:37:34 +0100

    Update FAST_n_BRIEF.py

--- FAST_n_BRIEF.py
+++ FAST_n_BRIEF.py
@@ -14,11 +14,11 @@ def FAST_n_BRIEF(img1, img2, n=1000, threshold=40):
     kp1=fast.detect(test1,None)
     kp2=fast.detect(test2,None)
 
-    mark1=cv2.drawKeypoints(test1,kp1,None,color=(0,0,255),flags=0)
-    orient1=cv2.drawKeypoints(test1,kp1,None,color=(0,0,255),flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    #mark1=cv2.drawKeypoints(test1,kp1,None,color=(0,0,255),flags=0)
+    #orient1=cv2.drawKeypoints(test1,kp1,None,color=(0,0,255),flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-    mark2=cv2.drawKeypoints(test2,kp2,None,color=(255,0,0),flags=0)
-    orient2=cv2.drawKeypoints(test2,kp2,None,color=(255,0,0),flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    #mark2=cv2.drawKeypoints(test2,kp2,None,color=(255,0,0),flags=0)
+    #orient2=cv2.drawKeypoints(test2,kp2,None,color=(255,0,0),flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     brisk = cv2.BRISK_create(thresh=75)
     kp1, des1 = brisk.compute(test1, kp1)
@@ -28,8 +28,10 @@ def FAST_n_BRIEF(img1, img2, n=1000, threshold=40):
     matches = bf.match(des1,des2)
 
     matches = sorted(matches, key = lambda x:x.distance)
-    result = cv2.drawMatches(mark1,kp1,mark2,kp2,matches[:min(n, len(matches))],None,matchColor=[0,255,0], flags=2)
+    #result = cv2.drawMatches(mark1,kp1,mark2,kp2,matches[:min(n, len(matches))],None,matchColor=[0,255,0], flags=2)
 
-    plt.imshow(result, interpolation = 'bicubic')
-    plt.axis('off')
-    plt.show()
+    #plt.imshow(result, interpolation = 'bicubic')
+    #plt.axis('off')
+    #plt.show()
+    
+    return kp1, kp2, matches
